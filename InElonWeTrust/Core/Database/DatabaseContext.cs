24249f41867b91edbf157e52221bc4e87c35bcb3
tearth <Tearth@users.noreply.github.com>
2018-07-01 16:57:03 +0200

    Add database and EF.

---
+++ InElonWeTrust/Core/Database/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InElonWeTrust.Core.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base(GetOptions("Data Source=Database.sqlite"))
+        {
+
+        }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            return SqliteDbContextOptionsBuilderExtensions.UseSqlite(new DbContextOptionsBuilder(), connectionString).Options;
+        }
+    }
+}
