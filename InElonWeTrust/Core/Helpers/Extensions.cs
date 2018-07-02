1b2183ffbb140b9a2af4043fedefefe316d00e47
tearth <Tearth@users.noreply.github.com>
2018-07-02 19:43:19 +0200

    Add method to randomize item from list.

---
+++ InElonWeTrust/Core/Helpers/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InElonWeTrust.Core.Helpers
+{
+    public static class Extensions
+    {
+        private static Random _random;
+
+        static Extensions()
+        {
+            _random = new Random();
+        }
+
+        public static T GetRandomItem<T>(this List<T> enumerable)
+        {
+            var index = _random.Next(0, enumerable.Count() - 1);
+            return enumerable[index];
+        }
+    }
+}
