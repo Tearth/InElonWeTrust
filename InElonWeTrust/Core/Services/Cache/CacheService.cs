ec19e04d7221d9218ef4e6c5794cfea68b6a5a98
tearth <Tearth@users.noreply.github.com>
2018-07-11 22:07:10 +0200

    Add cache usage reporting.

--- InElonWeTrust/Core/Services/Cache/CacheService.cs
+++ InElonWeTrust/Core/Services/Cache/CacheService.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Timers;
+using NLog;
 
 namespace InElonWeTrust.Core.Services.Cache
 {
     public class CacheService<T, D>
     {
         private Dictionary<T, CacheItem<D>> _items;
-        private const int CacheItemLifeLength = 60;
+        private Timer _cacheStatsTimer;
+        private int _cacheItemsAdded;
+        private int _cacheItemsHitted;
+        private int _cacheItemsUpdated;
+
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private const int CacheItemLifeLengthMinutes = 30;
+        private const int IntervalMinutes = 15;
 
         public CacheService()
         {
             _items = new Dictionary<T, CacheItem<D>>();
+
+            _cacheStatsTimer = new Timer(IntervalMinutes * 60 * 1000);
+            _cacheStatsTimer.Elapsed += CacheStatsTimerOnElapsed;
+            _cacheStatsTimer.Start();
         }
 
         public async Task<D> GetAndUpdateAsync(T type, Func<Task<D>> dataProviderDelegate)
@@ -21,17 +35,29 @@ namespace InElonWeTrust.Core.Services.Cache
                 var data = await dataProviderDelegate();
                 _items.Add(type, new CacheItem<D>(data));
 
+                _cacheItemsAdded++;
                 return data;
             }
 
             var cachedItem = _items[type];
-            if ((DateTime.Now - cachedItem.UpdateTime).TotalSeconds >= CacheItemLifeLength)
+            if ((DateTime.Now - cachedItem.UpdateTime).TotalMinutes >= CacheItemLifeLengthMinutes)
             {
                 var data = await dataProviderDelegate();
                 cachedItem.Update(data);
+
+                _cacheItemsUpdated++;
+            }
+            else
+            {
+                _cacheItemsHitted++;
             }
 
             return cachedItem.Data;
+        }
+
+        private void CacheStatsTimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            _logger.Info($"Cache stats for {typeof(T).Name}: {_cacheItemsAdded} added, {_cacheItemsUpdated} updated, {_cacheItemsHitted} hitted");
         }
     }
 }
