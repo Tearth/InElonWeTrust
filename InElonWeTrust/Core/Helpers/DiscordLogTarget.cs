220610502009f4e7d1b260ea1a15e6afa14d0b1b
tearth <Tearth@users.noreply.github.com>
2018-07-12 18:30:37 +0200

    Fix bug when log was too big for DM.

--- InElonWeTrust/Core/Helpers/DiscordLogTarget.cs
+++ InElonWeTrust/Core/Helpers/DiscordLogTarget.cs
@@ -1,4 +1,6 @@
-﻿using InElonWeTrust.Core.Settings;
+﻿using System;
+using DSharpPlus.Entities;
+using InElonWeTrust.Core.Settings;
 using NLog;
 using NLog.Targets;
 
@@ -12,8 +14,11 @@ namespace InElonWeTrust.Core.Helpers
 
             if (Bot.Client != null)
             {
+                var embed = new DiscordEmbedBuilder {Color = new DiscordColor(Constants.EmbedErrorColor)};
+                embed.AddField("Error", logMessage.Substring(0, Math.Min(1024, logMessage.Length)));
+
                 var dm = await Bot.Client.CreateDmAsync(await Bot.Client.GetUserAsync(SettingsLoader.Data.OwnerId));
-                await dm.SendMessageAsync(logMessage);
+                await dm.SendMessageAsync("", false, embed);
             }
         }
     }
