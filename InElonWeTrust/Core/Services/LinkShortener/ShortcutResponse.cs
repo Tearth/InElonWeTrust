69d70558aaffd738f979d76dc3e17b3c7b751bd0
tearth <Tearth@users.noreply.github.com>
2018-07-12 17:51:51 +0200

    Remove shortener service (embed supports this already).

--- InElonWeTrust/Core/Services/LinkShortener/ShortcutResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InElonWeTrust.Core.Services.LinkShortener
-{
-    public class ShortcutResponse
-    {
-        public ShortcutResponseData Data { get; set; }
-        public bool Success { get; set; }
-        public int Status { get; set; }
-    }
-}
