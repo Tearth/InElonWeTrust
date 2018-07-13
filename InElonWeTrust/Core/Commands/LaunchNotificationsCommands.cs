fd383cb7a7785f6cd7e43165a953eade0e665056
tearth <Tearth@users.noreply.github.com>
2018-07-13 22:11:00 +0200

    Add launch notifications displaying.

--- InElonWeTrust/Core/Commands/LaunchNotificationsCommands.cs
+++ InElonWeTrust/Core/Commands/LaunchNotificationsCommands.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
@@ -20,6 +23,8 @@ namespace InElonWeTrust.Core.Commands
         {
             _subscriptions = new SubscriptionsService();
             _launchNotifications = new LaunchNotificationsService();
+
+            _launchNotifications.OnLaunchNoification += LaunchNotificationsOnOnLaunchNoification;
         }
 
         [Command("SubscribeLaunchNotifications")]
@@ -105,6 +110,61 @@ namespace InElonWeTrust.Core.Commands
             }
 
             await ctx.RespondAsync("", false, embed);
+        }
+
+
+        private async void LaunchNotificationsOnOnLaunchNoification(object sender, LaunchNotification launchNotification)
+        {
+            var oldLaunchState = launchNotification.OldLaunchState;
+            var launch = launchNotification.NewLaunchState;
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Color = new DiscordColor(Constants.EmbedColor),
+                ThumbnailUrl = launch.Links.MissionPatch ?? Constants.SpaceXLogoImage
+            };
+
+            var timeLeft = (launch.LaunchDateUtc - DateTime.Now.ToUniversalTime()).Value.TotalMinutes;
+
+            switch (launchNotification.Type)
+            {
+                case LaunchNotificationType.Reminder:
+                {
+                    var timeLeftDescription = timeLeft > 60 ? Math.Ceiling(timeLeft / 60) + "hours" : Math.Ceiling(timeLeft) + " minutes";
+
+                    var descriptionBuilder = new StringBuilder();
+                    descriptionBuilder.Append($"**{timeLeftDescription}** to launch {launch.MissionName}! ");
+                    descriptionBuilder.Append($"Type `e!nextlaunch` or `e!getlaunch {launch.FlightNumber.Value}` to get more information.");
+
+                    embed.AddField(":rocket: Launch is upcoming!", descriptionBuilder.ToString());
+                    break;
+                }
+
+                case LaunchNotificationType.Scrub:
+                {
+                    var descriptionBuilder = new StringBuilder();
+                    descriptionBuilder.Append($"**{launch.MissionName}** launch time has been changed from " +
+                                              $"**{oldLaunchState.LaunchDateUtc.Value.ToString("F", CultureInfo.InvariantCulture)}** to " +
+                                              $"**{launch.LaunchDateUtc.Value.ToString("F", CultureInfo.InvariantCulture)}**.");
+
+                    descriptionBuilder.Append($"Type `e!nextlaunch` or `e!getlaunch {launch.FlightNumber.Value}` to get more information.");
+
+                    embed.AddField(":warning: Scrub!", descriptionBuilder.ToString());
+                    break;
+                }
+            }
+
+            var channelIds = _subscriptions.GetSubscribedChannels(SubscriptionType.NextLaunch);
+            foreach (var channelId in channelIds)
+            {
+                var channel = await Bot.Client.GetChannelAsync(channelId);
+                await channel.SendMessageAsync("", false, embed);
+
+                if (launchNotification.Type == LaunchNotificationType.Reminder && timeLeft < 60 && launch.Links.VideoLink != null)
+                {
+                    await channel.SendMessageAsync($"**YouTube stream:** {launch.Links.VideoLink}");
+                }
+            }
         }
     }
 }
