ecbaee7d1c1a4e7409ef02292a3c116571af4296
tearth <Tearth@users.noreply.github.com>
2018-07-13 23:31:27 +0200

    Add uptime command.

--- InElonWeTrust/Core/Commands/PingCommand.cs
+++ InElonWeTrust/Core/Commands/PingCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using InElonWeTrust.Core.Attributes;
@@ -8,12 +9,27 @@ namespace InElonWeTrust.Core.Commands
     [Commands(":question:", "Misc", "Other strange commands")]
     public class PingCommand
     {
+        private DateTime _startTime;
+
+        public PingCommand()
+        {
+            _startTime = DateTime.Now;
+        }
+
         [Command("Ping")]
         [Description("Ping command")]
         public async Task Ping(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
             await ctx.RespondAsync($"Ping: {ctx.Client.Ping} ms");
+        }
+
+        [Command("Uptime")]
+        [Description("Get uptime")]
+        public async Task Uptime(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync($"Uptime: {DateTime.Now - _startTime:g}");
         }
     }
 }
