7d2b30a437bdeddf91f5aa1b96d3f9b14444165a
tearth <Tearth@users.noreply.github.com>
2018-07-13 22:10:45 +0200

    Add methods to generate launch notifications.

---
+++ InElonWeTrust/Core/Services/LaunchNotifications/LaunchNotification.cs
@@ -0,0 +1,18 @@
+﻿using Oddity.API.Models.Launch;
+
+namespace InElonWeTrust.Core.Services.LaunchNotifications
+{
+    public class LaunchNotification
+    {
+        public LaunchNotificationType Type { get; }
+        public LaunchInfo OldLaunchState { get; }
+        public LaunchInfo NewLaunchState { get; }
+
+        public LaunchNotification(LaunchNotificationType type, LaunchInfo oldLaunchState, LaunchInfo newLaunchState)
+        {
+            Type = type;
+            OldLaunchState = oldLaunchState;
+            NewLaunchState = newLaunchState;
+        }
+    }
+}
--- InElonWeTrust/Core/Services/LaunchNotifications/LaunchNotificationsService.cs
+++ InElonWeTrust/Core/Services/LaunchNotifications/LaunchNotificationsService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Timers;
 using NLog;
 using Oddity;
@@ -8,9 +10,12 @@ namespace InElonWeTrust.Core.Services.LaunchNotifications
 {
     public class LaunchNotificationsService
     {
+        public event EventHandler<LaunchNotification> OnLaunchNoification;
+
         private Timer _notificationsUpdateTimer;
         private OddityCore _oddity;
-        private LaunchInfo _nextLaunch;
+        private LaunchInfo _nextLaunchState;
+        private List<int> _notificationTimes;
 
         private Logger _logger = LogManager.GetCurrentClassLogger();
 
@@ -19,6 +24,7 @@ namespace InElonWeTrust.Core.Services.LaunchNotifications
         public LaunchNotificationsService()
         {
             _oddity = new OddityCore();
+            _notificationTimes = new List<int> { 2, 10, 60, 60 * 24, 60 * 24 * 7 };
 
             _notificationsUpdateTimer = new Timer(IntervalMinutes * 60 * 1000);
             _notificationsUpdateTimer.Elapsed += Notifications_UpdateTimerOnElapsed;
@@ -27,13 +33,36 @@ namespace InElonWeTrust.Core.Services.LaunchNotifications
 
         private async void Notifications_UpdateTimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
         {
-            if (_nextLaunch == null)
+            if (_nextLaunchState == null)
             {
-                _nextLaunch = await _oddity.Launches.GetNext().ExecuteAsync();
+                _nextLaunchState = await _oddity.Launches.GetNext().ExecuteAsync();
             }
             else
             {
+                var newLaunchState = await _oddity.Launches.GetNext().ExecuteAsync();
+                newLaunchState.LaunchDateUtc = new DateTime(2018, 7, 13, 21, 52, 10).ToUniversalTime();
 
+                if (newLaunchState.FlightNumber.Value == _nextLaunchState.FlightNumber.Value)
+                {
+                    if (newLaunchState.LaunchDateUtc != _nextLaunchState.LaunchDateUtc)
+                    {
+                        OnLaunchNoification?.Invoke(this, new LaunchNotification(LaunchNotificationType.Scrub, _nextLaunchState, newLaunchState));
+                        _logger.Info($"Scrub notification sent to {OnLaunchNoification.GetInvocationList().Length} channels");
+                    }
+                    else
+                    {
+                        var previousStateMinutesToLaunch = (newLaunchState.LaunchDateUtc - DateTime.Now.AddMinutes(-IntervalMinutes).ToUniversalTime()).Value.TotalMinutes;
+                        var newStateMinutesToLaunch = (newLaunchState.LaunchDateUtc - DateTime.Now.ToUniversalTime()).Value.TotalMinutes;
+
+                        if (_notificationTimes.Any(p => previousStateMinutesToLaunch >= p && newStateMinutesToLaunch < p))
+                        {
+                            OnLaunchNoification?.Invoke(this, new LaunchNotification(LaunchNotificationType.Reminder, _nextLaunchState, newLaunchState));
+                            _logger.Info($"Reminder notification sent to {OnLaunchNoification.GetInvocationList().Length} channels");
+                        }
+                    }
+                }
+
+                _nextLaunchState = newLaunchState;
             }
         }
     }
