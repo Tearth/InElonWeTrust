62e7d6302e543e438dfd477d91ef33540a023122
tearth <Tearth@users.noreply.github.com>
2018-07-16 00:11:00 +0200

    Update company info.

--- InElonWeTrust/Core/Commands/CompanyInfoCommand.cs
+++ InElonWeTrust/Core/Commands/CompanyInfoCommand.cs
@@ -37,18 +37,20 @@ namespace InElonWeTrust.Core.Commands
                 ThumbnailUrl = Constants.SpaceXLogoImage
             };
 
-            embed.AddField(companyInfo.Name, companyInfo.Summary);
-            embed.AddField("CEO", companyInfo.Ceo, true);
-            embed.AddField("COO", companyInfo.Coo, true);
-            embed.AddField("CTO", companyInfo.Cto, true);
-            embed.AddField("CTO Propolusion", companyInfo.CtoPropulsion, true);
+            embed.AddField(companyInfo.Name, companyInfo.Summary + " [Read more on Wikipedia](https://en.wikipedia.org/wiki/SpaceX).");
+            embed.AddField("CEO", $"[{companyInfo.Ceo}](https://en.wikipedia.org/wiki/Elon_Musk)", true);
+            embed.AddField("COO", $"[{companyInfo.Coo}](https://en.wikipedia.org/wiki/Gwynne_Shotwell)", true);
+            embed.AddField("CTO", $"[{companyInfo.Cto}](https://en.wikipedia.org/wiki/Elon_Musk)", true);
+            embed.AddField("CTO Propolusion", $"[{companyInfo.CtoPropulsion}](https://en.wikipedia.org/wiki/Tom_Mueller)", true);
 
             embed.AddField("Founded year", companyInfo.FoundedYear.Value.ToString(), true);
             embed.AddField("Employees", companyInfo.Employees.ToString(), true);
             embed.AddField("Launch sites", companyInfo.LaunchSites.ToString(), true);
             embed.AddField("Vehicles", companyInfo.Vehicles.ToString(), true);
 
-            embed.AddField("Headquarters", $"{companyInfo.Headquarters.City}, {companyInfo.Headquarters.State}, {companyInfo.Headquarters.Address}");
+            embed.AddField("Headquarters",
+                $"[{companyInfo.Headquarters.City}, {companyInfo.Headquarters.State}, {companyInfo.Headquarters.Address}]" +
+                $"(https://www.google.com/maps/place/Rocket+Rd,+Hawthorne,+CA+90250,+Stany+Zjednoczone/@33.9213093,-118.3301254,17z/data=!3m1!4b1!4m5!3m4!1s0x80c2b5ded9a490b5:0x3095ae5795c500b3!8m2!3d33.9213093!4d-118.3279367)");
 
             await ctx.RespondAsync("", false, embed);
         }
