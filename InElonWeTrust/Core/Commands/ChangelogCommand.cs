96f9dd028fc1ef7c6a53b67c7636811441ec9535
tearth <Tearth@users.noreply.github.com>
2018-07-15 13:29:48 +0200

    Add changelog command.

---
+++ InElonWeTrust/Core/Commands/ChangelogCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using InElonWeTrust.Core.Attributes;
+using InElonWeTrust.Core.Helpers;
+using InElonWeTrust.Core.Services.Changelog;
+
+namespace InElonWeTrust.Core.Commands
+{
+    [Commands(":question:", "Misc", "Other strange commands")]
+    public class ChangelogCommand
+    {
+        private ChangelogService _changelogService;
+
+        public ChangelogCommand()
+        {
+            _changelogService = new ChangelogService();
+        }
+
+        [Command("Changelog")]
+        [Description("Get bot changelog.")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Color = new DiscordColor(Constants.EmbedColor),
+                ThumbnailUrl = Constants.ThumbnailImage
+            };
+
+            var changelog = await _changelogService.GetChangelog();
+            embed.AddField("Changelog", changelog);
+
+            await ctx.RespondAsync("", false, embed);
+        }
+    }
+}
---
+++ InElonWeTrust/Core/Services/Changelog/ChangelogService.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace InElonWeTrust.Core.Services.Changelog
+{
+    public class ChangelogService
+    {
+        public async Task<string> GetChangelog()
+        {
+            var httpClient = new HttpClient();
+            return await httpClient.GetStringAsync("https://raw.githubusercontent.com/Tearth/InElonWeTrust/master/CHANGELOG.md");
+        }
+    }
+}
