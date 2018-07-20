e6a6b821a67d8c0019779f56c94194819934511b
tearth <Tearth@users.noreply.github.com>
2018-07-20 21:36:33 +0200

    Move Twitter init to the separate task due to exception in DSharpPlus.

--- InElonWeTrust/Core/Services/Twitter/TwitterService.cs
+++ InElonWeTrust/Core/Services/Twitter/TwitterService.cs
@@ -34,13 +34,18 @@ namespace InElonWeTrust.Core.Services.Twitter
                 {TwitterUserType.SpaceX, "SpaceX"}
             };
 
-            var consumerKey = SettingsLoader.Data.TwitterConsumerKey;
-            var consumerSecret = SettingsLoader.Data.TwitterConsumerSecret;
-            var accessToken = SettingsLoader.Data.TwitterAccessToken;
-            var accessTokenSecret = SettingsLoader.Data.TwitterAccessTokenSecret;
+            Task.Run(() =>
+            {
+                Task.Delay(5000);
 
-            Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
-            InitStream();
+                var consumerKey = SettingsLoader.Data.TwitterConsumerKey;
+                var consumerSecret = SettingsLoader.Data.TwitterConsumerSecret;
+                var accessToken = SettingsLoader.Data.TwitterAccessToken;
+                var accessTokenSecret = SettingsLoader.Data.TwitterAccessTokenSecret;
+
+                Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+                InitStream();
+            });
         }
 
         private void InitStream()
