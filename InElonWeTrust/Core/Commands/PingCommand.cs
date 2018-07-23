1f2699858e8127d902e8716e46fd86eaf2fb7d3c
tearth <Tearth@users.noreply.github.com>
2018-07-23 17:26:25 +0200

    Move uptime command to the separate class.

--- InElonWeTrust/Core/Commands/PingCommand.cs
+++ InElonWeTrust/Core/Commands/PingCommand.cs
@@ -10,27 +10,12 @@ namespace InElonWeTrust.Core.Commands
     [Commands(GroupType.Miscellaneous)]
     public class PingCommand
     {
-        private readonly DateTime _startTime;
-
-        public PingCommand()
-        {
-            _startTime = DateTime.Now;
-        }
-
         [Command("Ping")]
         [Description("Ping command")]
         public async Task Ping(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
             await ctx.RespondAsync($"Ping: {ctx.Client.Ping} ms");
-        }
-
-        [Command("Uptime")]
-        [Description("Get uptime")]
-        public async Task Uptime(CommandContext ctx)
-        {
-            await ctx.TriggerTypingAsync();
-            await ctx.RespondAsync($"Uptime: {DateTime.Now - _startTime:g}");
         }
     }
 }
---
+++ InElonWeTrust/Core/Commands/UptimeCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using InElonWeTrust.Core.Attributes;
+using InElonWeTrust.Core.Commands.Definitions;
+
+namespace InElonWeTrust.Core.Commands
+{
+    [Commands(GroupType.Miscellaneous)]
+    public class UptimeCommand
+    {
+        private readonly DateTime _startTime;
+
+        public UptimeCommand()
+        {
+            _startTime = DateTime.Now;
+        }
+
+        [Command("Uptime")]
+        [Description("Get uptime")]
+        public async Task Uptime(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync($"Uptime: {DateTime.Now - _startTime:g}");
+        }
+    }
+}
