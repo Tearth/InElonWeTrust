93337fdac9c1750e29c683ee63db0f5fb910a714
tearth <Tearth@users.noreply.github.com>
2018-07-25 19:06:43 +0200

    Fix bug when exception in Discord target was crashing all bot.

--- InElonWeTrust/Core/Helpers/DiscordLogTarget.cs
+++ InElonWeTrust/Core/Helpers/DiscordLogTarget.cs
@@ -10,15 +10,23 @@ namespace InElonWeTrust.Core.Helpers
     {
         protected override async void Write(LogEventInfo logEvent)
         {
-            var logMessage = Layout.Render(logEvent);
-
-            if (Bot.Client != null)
+            try
             {
-                var embed = new DiscordEmbedBuilder {Color = new DiscordColor(Constants.EmbedErrorColor)};
-                embed.AddField("Error", logMessage.Substring(0, Math.Min(1024, logMessage.Length)));
+                var logMessage = Layout.Render(logEvent);
 
-                var dm = await Bot.Client.CreateDmAsync(await Bot.Client.GetUserAsync(SettingsLoader.Data.OwnerId));
-                await dm.SendMessageAsync("", false, embed);
+                if (Bot.Client != null)
+                {
+                    var embed = new DiscordEmbedBuilder { Color = new DiscordColor(Constants.EmbedErrorColor) };
+                    embed.AddField("Error", logMessage.Substring(0, Math.Min(1024, logMessage.Length)));
+
+                    var dm = await Bot.Client.CreateDmAsync(await Bot.Client.GetUserAsync(SettingsLoader.Data.OwnerId));
+                    await dm.SendMessageAsync("", false, embed);
+                }
+            }
+            catch (Exception e)
+            {
+                // Well, there is nothing more we can do here.
+                Console.WriteLine(e);
             }
         }
     }
