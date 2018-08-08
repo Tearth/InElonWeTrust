ed9e94fca7a7215ade88679d3e1040553b36aa0e
tearth <Tearth@users.noreply.github.com>
2018-08-08 18:56:54 +0200

    Add more info to Roadster command.

--- InElonWeTrust/Core/EmbedGenerators/RoadsterEmbedBuilder.cs
+++ InElonWeTrust/Core/EmbedGenerators/RoadsterEmbedBuilder.cs
@@ -20,12 +20,12 @@ namespace InElonWeTrust.Core.EmbedGenerators
 
             var embedBuilder = new DiscordEmbedBuilder
             {
-                Title = roadster.Name,
-                Url = "http://www.whereisroadster.com/",
                 Color = new DiscordColor(Constants.EmbedColor),
                 ImageUrl = StarmanImageUrl,
                 Footer = new DiscordEmbedBuilder.EmbedFooter {Text = $"Roadster has been launched {daysFromLaunch} days ago."}
             };
+
+            embedBuilder.AddField(roadster.Name, $"{roadster.Details} [[Wikipedia]]({roadster.Wikipedia})");
 
             embedBuilder.AddField("Speed", $"{roadster.SpeedKph:# ### ###}  kph", true);
             embedBuilder.AddField("Period", $"{(int)roadster.PeriodDays} days", true);
