f8cd6c752a8749bd863e5f776fe02c157f0b8b95
tearth <tearthdev@gmail.com>
2019-04-12 15:58:01 +0200

    Fix invalid links to bot list api.

--- InElonWeTrust/Core/Services/BotLists/BotListsService.cs
+++ InElonWeTrust/Core/Services/BotLists/BotListsService.cs
@@ -21,9 +21,9 @@ namespace InElonWeTrust.Core.Services.BotLists
             {
                 new BotListUpdater("https://discordbots.org/api/bots/{0}/stats", "server_count", SettingsLoader.Data.DiscordBotsToken),
                 new BotListUpdater("https://botsfordiscord.com/api/bot/{0}", "server_count", SettingsLoader.Data.BotsForDiscordToken),
-                new BotListUpdater("https://botlist.space/api/bots/{0}", "server_count", SettingsLoader.Data.BotlistToken),
+                new BotListUpdater("https://api.botlist.space/v1/bots/{0}", "server_count", SettingsLoader.Data.BotlistToken),
                 new BotListUpdater("https://bots.discord.pw/api/bots/{0}/stats", "server_count", SettingsLoader.Data.DiscordPwToken),
-                new BotListUpdater("https://discordsbestbots.xyz/api/bots/{0}", "guilds", SettingsLoader.Data.DiscordsBestBotsToken),
+                new BotListUpdater("https://discordsbestbots.xyz/api/bots/{0}/stats", "guilds", SettingsLoader.Data.DiscordsBestBotsToken),
                 new BotListUpdater("https://discordbot.world/api/bot/{0}/stats", "guild_count", SettingsLoader.Data.DiscordBotWorldToken),
                 new BotListUpdater("https://discordbots.group/api/bot/{0}", "count", SettingsLoader.Data.DiscordBotsGroupToken),
                 new BotListUpdater("https://discordbotlist.com/api/bots/{0}/stats", "guilds", "Bot " + SettingsLoader.Data.DiscordBotListToken),
