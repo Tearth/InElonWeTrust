370f125db29c5f4dd31b6e914f4aa4203e265362
tearth <tearthdev@gmail.com>
2019-06-27 15:03:19 +0200

    Add "Say" command (only for owner).

---
+++ InElonWeTrust/Core/Commands/SayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using InElonWeTrust.Core.Attributes;
+using InElonWeTrust.Core.Commands.Definitions;
+using InElonWeTrust.Core.Settings;
+
+namespace InElonWeTrust.Core.Commands
+{
+    [Commands(GroupType.Miscellaneous)]
+    public class SayCommand : BaseCommandModule
+    {
+        [Command("Say")]
+        [Description("Say something as Elon")]
+        [HiddenCommand]
+        public async Task Say(CommandContext ctx, string content)
+        {
+            if (ctx.User.Id != SettingsLoader.Data.OwnerId)
+            {
+                return;
+            }
+
+            await ctx.Message.DeleteAsync();
+            await ctx.RespondAsync(content);
+        }
+    }
+}
