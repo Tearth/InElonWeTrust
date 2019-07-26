27b25593cc904d6c4e0c84eac20f74d6efaa8608
tearth <tearthdev@gmail.com>
2019-07-26 23:35:27 +0200

    Add method to generate Google Maps link.

---
+++ InElonWeTrust/Core/Helpers/Formatters/GoogleMapsLinkFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace InElonWeTrust.Core.Helpers.Formatters
+{
+    public static class GoogleMapsLinkFormatter
+    {
+        public static string GetGoogleMapsLink(float latitude, float longitude)
+        {
+            var numberFormat = new NumberFormatInfo() {NumberDecimalSeparator = "."};
+            var latitudeString = latitude.ToString(numberFormat);
+            var longitudeString = longitude.ToString(numberFormat);
+
+            return $"https://maps.google.com/maps?q={latitudeString},{longitudeString}&t=k";
+        }
+    }
+}
