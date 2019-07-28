fb176279a24decb8b7d416b33e3167354328e13e
tearth <tearthdev@gmail.com>
2019-07-28 14:56:42 +0200

    Fix style issues.

--- InElonWeTrust/Core/Helpers/Formatters/GoogleMapsLinkFormatter.cs
+++ InElonWeTrust/Core/Helpers/Formatters/GoogleMapsLinkFormatter.cs
@@ -6,7 +6,7 @@ namespace InElonWeTrust.Core.Helpers.Formatters
     {
         public static string GetGoogleMapsLink(float latitude, float longitude)
         {
-            var numberFormat = new NumberFormatInfo() {NumberDecimalSeparator = "."};
+            var numberFormat = new NumberFormatInfo {NumberDecimalSeparator = "."};
             var latitudeString = latitude.ToString(numberFormat);
             var longitudeString = longitude.ToString(numberFormat);
 
