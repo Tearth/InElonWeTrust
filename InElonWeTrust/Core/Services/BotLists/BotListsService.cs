262e73bb34d111aa74023ad53daeef9a61873f84
tearth <tearthdev@gmail.com>
2019-09-08 15:28:04 +0200

    Enable discordbestbots in service.

--- InElonWeTrust/Core/Services/BotLists/BotListsService.cs
+++ InElonWeTrust/Core/Services/BotLists/BotListsService.cs
@@ -23,7 +23,7 @@ namespace InElonWeTrust.Core.Services.BotLists
                 new BotListUpdater("https://discordbots.org/api/bots/{0}/stats", "server_count", SettingsLoader.Data.DiscordBotsToken),
                 new BotListUpdater("https://botsfordiscord.com/api/bot/{0}", "server_count", SettingsLoader.Data.BotsForDiscordToken),
                 new BotListUpdater("https://api.botlist.space/v1/bots/{0}", "server_count", SettingsLoader.Data.BotlistToken),
-                //new BotListUpdater("https://discordsbestbots.xyz/api/bots/{0}/stats", "guilds", SettingsLoader.Data.DiscordsBestBotsToken),
+                new BotListUpdater("https://discordsbestbots.xyz/api/bots/{0}/stats", "guilds", SettingsLoader.Data.DiscordsBestBotsToken),
                 new BotListUpdater("https://discordbot.world/api/bot/{0}/stats", "guild_count", SettingsLoader.Data.DiscordBotWorldToken),
                 new BotListUpdater("https://discordbotlist.com/api/bots/{0}/stats", "guilds", "Bot " + SettingsLoader.Data.DiscordBotListToken),
                 new BotListUpdater("https://bots.ondiscord.xyz/bot-api/bots/{0}/guilds", "guildCount", SettingsLoader.Data.BotsOnDiscordToken),
