3f73a976d8c0edb9313e8bb6948c916abf5a860c
Tearth <tearthdev@gmail.com>
2020-05-30 01:32:59 +0200

    Add SN command (without dynamic data yet)

---
+++ InElonWeTrust/Core/Commands/SnCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using InElonWeTrust.Core.Commands.Attributes;
+using InElonWeTrust.Core.Commands.Definitions;
+using InElonWeTrust.Core.EmbedGenerators;
+using InElonWeTrust.Core.Services.Sn;
+using InElonWeTrust.Core.Services.Twitter;
+
+namespace InElonWeTrust.Core.Commands
+{
+    [CommandsGroup(GroupType.Miscellaneous)]
+    public class SnCommand : BaseCommandModule
+    {
+        private readonly SnService _snService;
+
+        public SnCommand(SnService snService)
+        {
+            _snService = snService;
+        }
+
+        [Command("Sn")]
+        [Description("Get the latest SN status.")]
+        public async Task AvatarAsync(CommandContext ctx, int? number)
+        {
+            await ctx.TriggerTypingAsync();
+            var text = _snService.GetSnText();
+            await ctx.RespondAsync(text);
+        }
+    }
+}
